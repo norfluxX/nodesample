a5c75967f90ea0dfd7bafe65c964e0bb5ae5d199
Bhikesh Khute <35907619+norfluxX@users.noreply.github.com>
2023-12-31 07:32:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //now it load express module with `require` directive
 var express = require('express')
 var app = express()
+const port = process.env.PORT || 3000;
 //Define request response in root URL (/) and response with text Hello World!
 app.get('/', function (req, res) {
   res.send('Hello Railway !')
 })
 //Launch listening server on port 8080 and consoles the log.
-app.listen(3000, function () {
-  console.log('app listening on port 3000!')
-})
+app.listen(port, () => {
+  console.log(`Server running at https://localhost:${port}/`);
+});
