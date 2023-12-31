c262bba3f7faee59b7bd3ea8e9bb740e262bed44
Bhikesh Khute <35907619+norfluxX@users.noreply.github.com>
2023-12-31 07:09:41 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+//now it load express module with `require` directive
+var express = require('express')
+var app = express()
+//Define request response in root URL (/) and response with text Hello World!
+app.get('/', function (req, res) {
+  res.send('Hello Railway !')
+})
+//Launch listening server on port 8080 and consoles the log.
+app.listen(3000, function () {
+  console.log('app listening on port 3000!')
+})
